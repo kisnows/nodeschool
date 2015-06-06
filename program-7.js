7c7218cf00cae956c2df657fe79c8bc5dcf93c79
yuanqi <yq12315@gmail.com>
2015-06-07 00:28:36 +0800

    add 7-10

--- program-7.js
+++ program-7.js
@@ -1,43 +1,31 @@
 var http = require('http');
-var url = process.argv[2];
 
-http.get(url,function(response){
-    var list = [];
-    // response.setEncoding('utf8');
+http.get(process.argv[2],function(response){
+    response.setEncoding('utf8');
     response.on("data",function(data){
-        list.push(data.toString());
-    });
+        console.log(data);
+    })
     response.on("error",function(err){
-        throw err;
-    });
+        throw err
+    })
     response.on("end",function(){
-        var str = '';
-        list.forEach(function(s){
-            str += s;
-        });
-        console.log(str.length);
-        console.log(str);
-    });
-});
+        // console.log(date);
+    })
+})
 
 
 /*以下是官方给出的参考答案，以备你做比较参考:
 
-──────────────────────────────
-──────────────────────────────
+─────────────────────────────
+─────────────────────────────
 
     var http = require('http')
-    var bl = require('bl')
 
     http.get(process.argv[2], function (response) {
-      response.pipe(bl(function (err, data) {
-        if (err)
-          return console.error(err)
-        data = data.toString()
-        console.log(data.length)
-        console.log(data)
-      }))
+      response.setEncoding('utf8')
+      response.on('data', console.log)
+      response.on('error', console.error)
     })
 
-──────────────────────────────
-──────────────────────────────*/
+─────────────────────────────
+─────────────────────────────*/
--- program-8.js
+++ program-8.js
@@ -1,81 +1,43 @@
 var http = require('http');
-var urls = process.argv.slice(2,process.argv.length);
-var datas = [];
-var end = [];
-var is = false;
-urls.forEach(function(url,index){
-    http.get(url,function(response){
-        var list = [];
-        response.setEncoding('utf8');
-        response.on("data",function(data){
-            list.push(data);
-        });
-        response.on("error",function(err){
-            throw err;
-        });
-        response.on("end",function(){
-            var str = '';
-            list.forEach(function(s){
-                str += s;
-            });
-            datas[index] = str;
-            end.push(true);
-            if (isEnd()) {
-                for (var i = 0; i < datas.length; i++) {
-                    console.log(datas[i])
-                }
-            }
-        });
+var url = process.argv[2];
+
+http.get(url,function(response){
+    var list = [];
+    // response.setEncoding('utf8');
+    response.on("data",function(data){
+        list.push(data.toString());
     });
-
-});
-function isEnd(){
-    if (end.length === urls.length) {
-        end.forEach(function(blo){
-            if(blo){
-                is = true;
-            }else{
-                is = false;
-            }
+    response.on("error",function(err){
+        throw err;
+    });
+    response.on("end",function(){
+        var str = '';
+        list.forEach(function(s){
+            str += s;
         });
-    }
-    return is;
-}
-
+        console.log(str.length);
+        console.log(str);
+    });
+});
 
 
 /*以下是官方给出的参考答案，以备你做比较参考:
 
-───────────────────────────────
-───────────────────────────────
+──────────────────────────────
+──────────────────────────────
 
     var http = require('http')
     var bl = require('bl')
-    var results = []
-    var count = 0
 
-    function printResults () {
-      for (var i = 0; i < 3; i++)
-        console.log(results[i])
-    }
+    http.get(process.argv[2], function (response) {
+      response.pipe(bl(function (err, data) {
+        if (err)
+          return console.error(err)
+        data = data.toString()
+        console.log(data.length)
+        console.log(data)
+      }))
+    })
 
-    function httpGet (index) {
-      http.get(process.argv[2 + index], function (response) {
-        response.pipe(bl(function (err, data) {
-          if (err)
-            return console.error(err)
-
-          results[index] = data.toString()
-          count++
-
-          if (count == 3)
-            printResults()
-        }))
-      })
-    }
-
-    for (var i = 0; i < 3; i++)
-      httpGet(i)
-
-───────────────────────────────
-───────────────────────────────*/
+──────────────────────────────
+──────────────────────────────*/
