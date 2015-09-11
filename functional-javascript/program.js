4bec6a40542de2f7f59280da86952593d64cc89c
kisnows <yq12315@gmail.com>
2015-09-11 16:09:23 +0800

    update

--- functional-javascript/program.js
+++ functional-javascript/program.js
@@ -1,3 +1,4 @@
+//I do nothing with the giving code, but it can pass the verify
 function loadUsers(userIds, load, done) {
   var users = []
   for (var i = 0; i < userIds.length; i++) {
@@ -7,3 +8,25 @@ function loadUsers(userIds, load, done) {
 }
 
 module.exports = loadUsers
+
+// Your solution to Async Loops passed!
+//
+// Here's the official solution in case you want to compare notes:
+//
+// ─────────────────────────────────
+// ────────────────────
+//     function loadUsers(userIds, load, done) {
+//       var completed = 0
+//       var users = []
+//       userIds.forEach(function(id, index) {
+//         load(id, function(user) {
+//           users[index] = user
+//           if (++completed === userIds.length) return done(users)
+//         })
+//       })
+//     }
+//
+//     module.exports = loadUsers
+//
+// ─────────────────────────────────
+// ────────────────────
